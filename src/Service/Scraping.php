8af938349cd1e08d1b3c00f87e8757e991cfc9b0
Matthieu <parismatthieu@yahoo.fr>
2020-11-26 09:18:30 +0100

    Fix extract

--- src/Service/Scraping.php
+++ src/Service/Scraping.php
@@ -19,36 +19,45 @@ class Scraping
 
     public function scrapAMD()
     {
+        // On récupère la date d'aujourd'hui
         $now = new \DateTime();
+        // On garde l'année
         $yearNow = (int)$now->format('Y');
         $start = 2018;
+        // Création de notre entité pour scraper
         $newClient = new Client();
+        // Parcours de chaque page par année
         while ($start <= $yearNow) {
             $crawler = $newClient->request('GET', "https://www.techpowerup.com/cpu-specs/?mfgr=AMD&released={$start}&sort=released");
-
+            // On récupère les balises tr de la page puis on les parcours
             $crawler->filter('tr')->each(function ($node) {
+                // On stock dans notre variable la ligne du tableau concernée
                 $string = ltrim($node->filter('tr')->text());
+                // On divise notre string en array
                 $array = explode("\n", $string);
                 if (count($array) >= 10) {
                     if ($array[0] != 'Name') {
+                        // On cherche dans notre bdd si le produit existe déjà
                         $proc = $this->em->getRepository(Cpu::class)->findBy(['product_name' => $array[0]]);
                         if (!$proc) {
+                            // Création d'un processeur
                             $newProc = new Cpu();
-                            $newProc->setProductName($array[0]);
-                            $newProc->setCodeName($array[2]);
+                            $newProc->setProductName(ltrim($array[0]));
+                            $newProc->setCodeName(ltrim($array[2]));
                             $newProc->setCompany('AMD');
-                            $newProc->setCores($array[3]);
-                            $newProc->setClock($array[4]);
-                            $newProc->setSocket($array[5]);
-                            $newProc->setProcess($array[6]);
-                            $newProc->setL3Cache($array[7]);
-                            $newProc->setTdp($array[8]);
-                            $newProc->setReleased($array[9]);
+                            $newProc->setCores(ltrim($array[3]));
+                            $newProc->setClock(ltrim($array[4]));
+                            $newProc->setSocket(ltrim($array[5]));
+                            $newProc->setProcess(ltrim($array[6]));
+                            $newProc->setL3Cache(ltrim($array[7]));
+                            $newProc->setTdp(ltrim($array[8]));
+                            $newProc->setReleased(ltrim($array[9]));
                             $this->em->persist($newProc);
                         }
                     }
                 }
             });
+            // Incrémentation de la date
             $start++;
         }
         $this->em->flush();
@@ -57,36 +66,45 @@ class Scraping
 
     public function scrapIntel()
     {
+        // On récupère la date d'aujourd'hui
         $now = new \DateTime();
+        // On garde l'année
         $yearNow = (int)$now->format('Y');
         $start = 2018;
+        // Création de notre entité pour scraper
         $newClient = new Client();
+        // Parcours de chaque page par année
         while ($start <= $yearNow) {
-
             $crawler = $newClient->request('GET', "https://www.techpowerup.com/cpu-specs/?mfgr=Intel&released={$start}&sort=released");
+            // On récupère les balises tr de la page puis on les parcours
             $crawler->filter('tr')->each(function ($node) {
+                // On stock dans notre variable la ligne du tableau concernée
                 $string = ltrim($node->filter('tr')->text());
+                // On divise notre string en array
                 $array = explode("\n", $string);
                 if (count($array) >= 10) {
                     if ($array[0] != 'Name') {
+                        // On cherche dans notre bdd si le produit existe déjà
                         $proc = $this->em->getRepository(Cpu::class)->findBy(['product_name' => $array[0]]);
                         if (!$proc) {
+                            // Création d'un processeur
                             $newProc = new Cpu();
-                            $newProc->setProductName($array[0]);
-                            $newProc->setCodeName($array[2]);
+                            $newProc->setProductName(ltrim($array[0]));
+                            $newProc->setCodeName(ltrim($array[2]));
                             $newProc->setCompany('Intel');
-                            $newProc->setCores($array[3]);
-                            $newProc->setClock($array[4]);
-                            $newProc->setSocket($array[5]);
-                            $newProc->setProcess($array[6]);
-                            $newProc->setL3Cache($array[7]);
-                            $newProc->setTdp($array[8]);
-                            $newProc->setReleased($array[9]);
+                            $newProc->setCores(ltrim($array[3]));
+                            $newProc->setClock(ltrim($array[4]));
+                            $newProc->setSocket(ltrim($array[5]));
+                            $newProc->setProcess(ltrim($array[6]));
+                            $newProc->setL3Cache(ltrim($array[7]));
+                            $newProc->setTdp(ltrim($array[8]));
+                            $newProc->setReleased(ltrim($array[9]));
                             $this->em->persist($newProc);
                         }
                     }
                 }
             });
+            // Incrémentation de la date
             $start++;
         }
         $this->em->flush();
@@ -95,35 +113,44 @@ class Scraping
 
     public function scrapGpuAMD()
     {
+        // On récupère la date d'aujourd'hui
         $now = new \DateTime();
+        // On garde l'année
         $yearNow = (int)$now->format('Y');
         $start = 2018;
+        // Création de notre entité pour scraper
         $newClient = new Client();
+        // Parcours de chaque page par année
         while ($start <= $yearNow) {
             $crawler = $newClient->request('GET', "https://www.techpowerup.com/gpu-specs/?mfgr=AMD&released={$start}&sort=released");
-
+            // On récupère les balises tr de la page puis on les parcours
             $crawler->filter('tr')->each(function ($node) {
+                // On stock dans notre variable la ligne du tableau concernée
                 $string = ltrim($node->filter('tr')->text());
+                // On divise notre string en array
                 $array = explode("\n", $string);
                 if (count($array) >= 10) {
                     if ($array[0] != 'Name') {
+                        // On cherche dans notre bdd si le produit existe déjà
                         $cg = $this->em->getRepository(Gpu::class)->findBy(['product_name' => $array[0]]);
                         if (!$cg) {
+                            // Création d'une carte graphique
                             $newcg = new Gpu();
-                            $newcg->setProductName($array[0]);
-                            $newcg->setGPUChip($array[4]);
+                            $newcg->setProductName(ltrim($array[0]));
+                            $newcg->setGPUChip(ltrim($array[4]));
                             $newcg->setCompany('AMD');
-                            $newcg->setReleaseDate($array[7]);
-                            $newcg->setBus($array[8]);
-                            $newcg->setMemory($array[9]);
-                            $newcg->setGPUclock($array[10]);
-                            $newcg->setMemoryclock($array[11]);
-                            $newcg->setShaders($array[12]);
+                            $newcg->setReleaseDate(ltrim($array[7]));
+                            $newcg->setBus(ltrim($array[8]));
+                            $newcg->setMemory(ltrim($array[9]));
+                            $newcg->setGPUclock(ltrim($array[10]));
+                            $newcg->setMemoryclock(ltrim($array[11]));
+                            $newcg->setShaders(ltrim($array[12]));
                             $this->em->persist($newcg);
                         }
                     }
                 }
             });
+            // Incrémentation de la date
             $start++;
         }
         $this->em->flush();
@@ -132,35 +159,44 @@ class Scraping
 
     public function scrapGpuIntel()
     {
+        // On récupère la date d'aujourd'hui
         $now = new \DateTime();
+        // On garde l'année
         $yearNow = (int)$now->format('Y');
         $start = 2018;
+        // Création de notre entité pour scraper
         $newClient = new Client();
+        // Parcours de chaque page par année
         while ($start <= $yearNow) {
-
             $crawler = $newClient->request('GET', "https://www.techpowerup.com/gpu-specs/?mfgr=Intel&released={$start}&sort=released");
+            // On récupère les balises tr de la page puis on les parcours
             $crawler->filter('tr')->each(function ($node) {
+                // On stock dans notre variable la ligne du tableau concernée
                 $string = ltrim($node->filter('tr')->text());
+                // On divise notre string en array
                 $array = explode("\n", $string);
                 if (count($array) >= 10) {
                     if ($array[0] != 'Name') {
+                        // On cherche dans notre bdd si le produit existe déjà
                         $cg = $this->em->getRepository(Gpu::class)->findBy(['product_name' => $array[0]]);
                         if (!$cg) {
+                            // Création d'une carte graphique
                             $newcg = new Gpu();
-                            $newcg->setProductName($array[0]);
-                            $newcg->setGPUChip($array[4]);
+                            $newcg->setProductName(ltrim($array[0]));
+                            $newcg->setGPUChip(ltrim($array[4]));
                             $newcg->setCompany('Intel');
-                            $newcg->setReleaseDate($array[7]);
-                            $newcg->setBus($array[8]);
-                            $newcg->setMemory($array[9]);
-                            $newcg->setGPUclock($array[10]);
-                            $newcg->setMemoryclock($array[11]);
-                            $newcg->setShaders($array[12]);
+                            $newcg->setReleaseDate(ltrim($array[7]));
+                            $newcg->setBus(ltrim($array[8]));
+                            $newcg->setMemory(ltrim($array[9]));
+                            $newcg->setGPUclock(ltrim($array[10]));
+                            $newcg->setMemoryclock(ltrim($array[11]));
+                            $newcg->setShaders(ltrim($array[12]));
                             $this->em->persist($newcg);
                         }
                     }
                 }
             });
+            // Incrémentation de la date
             $start++;
         }
         $this->em->flush();
@@ -169,35 +205,44 @@ class Scraping
 
     public function scrapGpuNvidia()
     {
+        // On récupère la date d'aujourd'hui
         $now = new \DateTime();
+        // On garde l'année
         $yearNow = (int)$now->format('Y');
         $start = 2018;
+        // Création de notre entité pour scraper
         $newClient = new Client();
+        // Parcours de chaque page par année
         while ($start <= $yearNow) {
-
             $crawler = $newClient->request('GET', "https://www.techpowerup.com/gpu-specs/?mfgr=NVIDIA&released={$start}&sort=released");
+            // On récupère les balises tr de la page puis on les parcours
             $crawler->filter('tr')->each(function ($node) {
+                // On stock dans notre variable la ligne du tableau concernée
                 $string = ltrim($node->filter('tr')->text());
+                // On divise notre string en array
                 $array = explode("\n", $string);
                 if (count($array) >= 10) {
                     if ($array[0] != 'Name') {
+                        // On cherche dans notre bdd si le produit existe déjà
                         $cg = $this->em->getRepository(Gpu::class)->findBy(['product_name' => $array[0]]);
                         if (!$cg) {
+                            // Création d'une carte graphique
                             $newcg = new Gpu();
-                            $newcg->setProductName($array[0]);
-                            $newcg->setGPUChip($array[4]);
+                            $newcg->setProductName(ltrim($array[0]));
+                            $newcg->setGPUChip(ltrim($array[4]));
                             $newcg->setCompany('NVIDIA');
-                            $newcg->setReleaseDate($array[7]);
-                            $newcg->setBus($array[8]);
-                            $newcg->setMemory($array[9]);
-                            $newcg->setGPUclock($array[10]);
-                            $newcg->setMemoryclock($array[11]);
-                            $newcg->setShaders($array[12]);
+                            $newcg->setReleaseDate(ltrim($array[7]));
+                            $newcg->setBus(ltrim($array[8]));
+                            $newcg->setMemory(ltrim($array[9]));
+                            $newcg->setGPUclock(ltrim($array[10]));
+                            $newcg->setMemoryclock(ltrim($array[11]));
+                            $newcg->setShaders(ltrim($array[12]));
                             $this->em->persist($newcg);
                         }
                     }
                 }
             });
+            // Incrémentation de la date
             $start++;
         }
         $this->em->flush();
